11747ea20399384e06f03103c3d36d7471f678a4
abuhrov <abuhrov@varteq.com>
2019-12-20 22:51:20 +0200

    small update

--- src/main/java/com/abuhrov/KickerRatingBot.java
+++ src/main/java/com/abuhrov/KickerRatingBot.java
@@ -4,6 +4,7 @@ import com.pengrad.telegrambot.BotUtils;
 import com.pengrad.telegrambot.TelegramBot;
 import com.pengrad.telegrambot.model.Message;
 import com.pengrad.telegrambot.model.Update;
+import com.pengrad.telegrambot.model.request.ReplyKeyboardMarkup;
 import com.pengrad.telegrambot.request.SendMessage;
 import spark.Request;
 import spark.Response;
@@ -11,30 +12,39 @@ import spark.Route;
 
 public class KickerRatingBot implements Route {
 
-	private static final String TOKEN = "877837908:AAGFnBbQp2sPQQG67WPmIdT8vXaFRLcIxio";
-	private final TelegramBot bot = new TelegramBot(TOKEN);
+    private static final String TOKEN = "877837908:AAGFnBbQp2sPQQG67WPmIdT8vXaFRLcIxio";
+    private final TelegramBot bot = new TelegramBot(TOKEN);
 
-	@Override
-	public Object handle(Request request, Response response) {
-		Update update = BotUtils.parseUpdate(request.body());
-		Message message = update.message();
-		Long chatId = message.chat().id();
-		System.out.println("handling: " + message.text());
+    @Override
+    public Object handle(Request request, Response response) {
+        Update update = BotUtils.parseUpdate(request.body());
+        Message message = update.message();
+        Long chatId = message.chat().id();
+        System.out.println("handling: " + message.text());
 
-		if ("newResult".equals(message.text())) {
-			bot.execute(new SendMessage(chatId, "new Result indeed"));
-		} else if ("showRating".equals(message.text())) {
-			bot.execute(new SendMessage(chatId, "show"));
-		}
+        if ("/newResult".equals(message.text())) {
+            bot.execute(new SendMessage(chatId, "").replyMarkup(new ReplyKeyboardMarkup(
+                    new String[]{"player1"},
+                    new String[]{"player2"},
+                    new String[]{"player3"}
+            )));
+        } else if ("/showRating".equals(message.text())) {
+            bot.execute(new SendMessage(chatId, """
+                    player1 - 2000
+                    player3 - 2400
+            """));
+        } else {
+            bot.execute(new SendMessage(chatId, "Користуйся краще готовими командами, я не настільки розумний.."));
+        }
 
-		return "ok";
-	}
+        return "ok";
+    }
 
-	String getToken() {
-		return TOKEN;
-	}
+    String getToken() {
+        return TOKEN;
+    }
 
-	TelegramBot getBot() {
-		return bot;
-	}
+    TelegramBot getBot() {
+        return bot;
+    }
 }
